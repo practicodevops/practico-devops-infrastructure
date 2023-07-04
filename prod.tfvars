4a3057ca34d3dad591ce440829690320ebf361ee
eliCazorla <eliascazorla2b@gmail.com>
2023-07-04 20:31:51 -0300

    se agrega resource eks cluster

--- prod.tfvars
+++ prod.tfvars
@@ -1,1 +1,4 @@
-environment = "production"
+environment       = "production"
+subnet_ids        = ["subnet-03fb97c7dd631f6ff", "subnet-066ca8a551d794947"]
+security_group    = "sg-0e467e8eaa7934a14"
+ec2_instance_type = "t3.small"
--- variables.tf
+++ variables.tf
@@ -1,4 +1,25 @@
 variable "environment" {
-    type = string
-    description = "Infrastructure environment"
+  type        = string
+  description = "Infrastructure environment"
+}
+
+variable "role_arn" {
+  type        = string
+  description = "Role arn"
+  default     = "arn:aws:iam::353129431097:role/LabRole"
+}
+
+variable "subnet_ids" {
+  type        = list(string)
+  description = "Subnet ids"
+}
+
+variable "security_group" {
+  type        = string
+  description = "Security group"
+}
+
+variable "ec2_instance_type" {
+  type        = string
+  description = "EC2 instance type"
 }
