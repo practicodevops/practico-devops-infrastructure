eeb16773cd1843cb2936e0d188614da3a85a66cf
eliCazorla <eliascazorla2b@gmail.com>
2023-07-17 21:41:20 -0300

    se divide resources en 2 archivos separados.

---
+++ eks-cluster.tf
@@ -0,0 +1,38 @@
+locals {
+  clustername = "eks-cluster-${var.environment}"
+}
+
+resource "aws_eks_cluster" "eks_cluster" {
+  name     = local.clustername
+  role_arn = var.role_arn
+
+  vpc_config {
+    subnet_ids         = var.subnet_ids
+    security_group_ids = [var.security_group]
+  }
+
+  tags = {
+    Environment = var.environment
+  }
+}
+
+resource "aws_eks_node_group" "node_group" {
+  cluster_name    = aws_eks_cluster.eks_cluster.name
+  node_group_name = "eks-cluster-node-group"
+  node_role_arn   = var.role_arn
+
+  subnet_ids = var.subnet_ids
+
+  scaling_config {
+    desired_size = 1
+    min_size     = 1
+    max_size     = 1
+  }
+
+  instance_types = [var.ec2_instance_type]
+  capacity_type  = "ON_DEMAND"
+
+  tags = {
+    Environment = var.environment
+  }
+}
